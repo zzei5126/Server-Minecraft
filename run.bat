37a7ad94079064ce91265f1794a68070d270119d
zzei5126 <nhuhieu5225@gmail.com>
2026-08-04 23:04:18 +0700

    Auto save Tue 08/04/2026 23:04:18.59

--- run.bat
+++ run.bat
@@ -1,1 +1,22 @@
-java -Xmx6G -Xms4G -jar server.jar nogui
+@echo off
+title Minecraft Server
+
+echo ===== Pulling from GitHub =====
+git pull
+
+echo.
+echo ===== Starting Server =====
+java -Xms2G -Xmx2G -jar server.jar nogui
+
+echo.
+echo ===== Server stopped =====
+
+git add .
+
+git commit -m "Auto save %date% %time%"
+
+git push
+
+echo.
+echo Done.
+pause
